f74d6906f54550d7530ff8a472cffa32e6358da2
Phantomlsh <phantomlsh@outlook.com>
2021-04-30 17:05:23 +0800

    Add App

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,23 +1,51 @@
 <template>
-  <div class="form">
-    <div class="is-flex is-align-items-center m-1" v-for="(n, p) in properties">
-      <label class="label m-0" style="min-width: 4rem;">{{ n[0] }}：</label>
-      <input class="input is-small" type="text" v-model="props.app[p]" :placeholder="n[1]">
+  <code class="ml-3">{{ app.id }}</code>
+  <div class="form m-2">
+    <div class="is-flex is-align-items-center m-3">
+      <label class="label m-0" style="min-width: 3.5rem;">id: </label>
+      <input class="input is-small" type="text" readonly="true" :value="app.id">
+    </div>
+    <div class="is-flex is-align-items-center m-3" v-for="(n, p) in properties">
+      <label class="label m-0" style="min-width: 3.5rem;">{{ n[0] }}：</label>
+      <input class="input is-small" type="text" v-model="app[p]" :placeholder="n[1]">
     </div>
     <label class="checkbox ml-3">
-      <input type="checkbox" v-model="props.app.secret">
+      <input type="checkbox" v-model="app.updateSecret">
       <span style="color: red; margin: 8px;">刷新secret</span>
     </label>
     <label class="checkbox ml-3">
-      <input type="checkbox" v-model="props.app.key">
+      <input type="checkbox" v-model="app.key">
       <span style="color: red; margin: 8px;">刷新RSA密钥对</span>
     </label>
+    <div class="buttons m-3">
+      <button class="button is-info" :class="{ 'is-loading': loading }" @click="submit" :disabled="!app.name">提交</button>
+      <button class="button is-danger" @click="remove"  :class="{ 'is-loading': loading }">删除</button>
+      <button class="button is-warning" @click="help">查看重要说明</button>
+    </div>
+    <div v-if="app.pk">
+      <label class="label">pk (public key)</label>
+      <textarea :value="app.pk" rows="9" readonly="true"></textarea>
+    </div>
+    <div v-if="app.sk">
+      <label class="label">sk (private key) CREDENTIAL</label>
+      <textarea :value="app.sk" rows="9" readonly="true"></textarea>
+    </div>
+    <div v-if="app.secret">
+      <label class="label">secret CREDENTIAL</label>
+      <textarea :value="app.secret" rows="2" readonly="true"></textarea>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { defineProps } from 'vue'
+import { defineProps, defineEmit, watchEffect } from 'vue'
+import axios from '../plugins/axios.js'
 const props = defineProps(['app'])
+const emit = defineEmit(['upsert', 'remove'])
+ref: app = props.app
+
+watchEffect(() => { app = props.app })
+
 const properties = {
   name: ['名称', '应用名称'],
   icon: ['图标', '图片链接地址'],
@@ -25,4 +53,72 @@ const properties = {
   platforms: ['平台', '允许登录的平台，默认全部允许'],
   token: ['令牌', '使用令牌登录，填写令牌模板']
 }
+
+ref: loading = false
+function help () {
+  const help = '只有<b>刷新</b>secret或者RSA密钥对以后，才可以查看最新secret或sk。'
+  Swal.fire('重要帮助信息', help, 'info')
+}
+
+async function submit () {
+  if (app.updateSecret || app.key) {
+    const res = await Swal.fire({
+      title: '确认提交',
+      text: '您正在尝试刷新secret或RSA密钥',
+      icon: 'warning',
+      showCancelButton: true
+    })
+    if (!res.isConfirmed) return
+  }
+  loading = true
+  const body = {
+    name: app.name,
+    icon: app.icon,
+    redirect: app.redirect,
+    platforms: app.platforms,
+    token: app.token,
+    secret: app.updateSecret,
+    key: app.key
+  }
+  await axios.post('/app/' + app.id, body, { headers: { token: window.sessionStorage.aauth } })
+    .then(({ data }) => {
+      app = data
+      Swal.fire('成功', '应用信息提交成功', 'success')
+    })
+    .catch(err => {
+      Swal.fire('错误', err.response ? err.response.data : '网络错误', 'error')
+    })
+  emit('upsert', app)
+  loading = false
+}
+
+async function remove () {
+  const res = await Swal.fire({
+    title: '确认删除',
+    text: '您正在尝试删除应用' + app.name,
+    icon: 'warning',
+    showCancelButton: true
+  })
+  if (!res.isConfirmed) return
+  loading = true
+  await axios.delete('/app/' + app.id, { headers: { token: window.sessionStorage.aauth } })
+    .then(({ data }) => {
+      Swal.fire('成功', '应用删除成功', 'success')
+    })
+    .catch(err => {
+      Swal.fire('错误', err.response ? err.response.data : '网络错误', 'error')
+    })
+  emit('remove', app)
+  loading = false
+}
 </script>
+
+<style scoped>
+textarea {
+  font-family: monospace;
+  width: 100%;
+  border: none;
+  background-color: white;
+  padding: 0px;
+}
+</style>
--- src/views/Admin.vue
+++ src/views/Admin.vue
@@ -1,35 +1,28 @@
 <template>
-  <div class="admin box">
-    <h1 v-if="!apps" class="title">正在载入...</h1>
-    <template v-else>
-      <h1 class="title">应用管理</h1>
-      <div class="app box p-2" v-for="(a, id) in apps" @click="edit = {...a}">
-        <div class="is-flex" style="align-items: center;">
-          <img style="max-width: 3rem; max-height: 3rem;" :src="a.icon || '/logo.png'" onerror="this.src = '/logo.png'">
-          <h3 class="title is-5 ml-3">{{ a.name }}</h3>
+  <div class="admin">
+    <h1 v-if="!apps" class="title m-4">正在载入...</h1>
+    <div v-else class="main" :style="mainStyle">
+      <div class="list p-2">
+        <h2 class="title is-4 m-3">应用列表</h2>
+        <button class="button is-info is-outlined is-fullwidth is-small" @click="select()">创建应用</button>
+        <div class="app m-2" v-for="(a, id) in apps" @click="select(id)">
+          <img style="max-width: 2rem; max-height: 2rem;" :src="a.icon || '/logo.png'" onerror="this.src = '/logo.png'">
+          <h3 class="title is-6">{{ a.name }}</h3>
         </div>
-        <code @click.stop="">{{ id }}</code>
       </div>
-    </template>
-  </div>
-  <div v-if="edit" class="modal is-clipped is-active">
-    <div class="modal-background" @click="edit = false"></div>
-    <div class="modal-card">
-      <header class="modal-card-head">
-        <p class="modal-card-title">编辑应用信息</p>
-      </header>
-      <section class="modal-card-body form">
-        <app :app="edit"></app>
-      </section>
-      <footer class="modal-card-foot">
-        <button class="button is-info">提交</button>
-      </footer>
+      <div class="edit p-2">
+        <h1 class="title m-3">
+          <i v-if="width <= 640" class="mdi mdi-24px mdi-menu" @click="open = !open"></i>
+          {{ edit ? '应用管理' : ''}}
+        </h1>
+        <app v-if="edit" :app="edit" @upsert="upsert" @remove="remove"></app>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
+import { computed } from 'vue'
 import { useRoute, useRouter } from 'vue-router'
 import axios from '../plugins/axios.js'
 import App from '../components/App.vue'
@@ -39,29 +32,83 @@ const SS = window.sessionStorage
 const token = () => ({ headers: { token: SS.aauth } })
 ref: apps = null
 ref: edit = null
+ref: open = true
+const width = window.innerWidth
+const mainStyle = computed(() => (open || width > 640) ? '' : 'width: calc(100% + 240px); left: -240px;')
 
 function catchErr (e) {
   console.log(e)
   Swal.fire('错误', e.response ? e.response.data : '网络错误', 'error')
 }
 
-console.log(route.query.state)
-if (route.query.token) router.push('/admin')
-else if (!SS.aauth) router.push('/')
-else axios.get('/app/', token())
-  .then(({ data }) => { apps = data })
-  .catch(catchErr)
+if (!SS.aauth) router.push('/')
+else {
+  if (route.query.token) router.push('/admin')
+  axios.get('/app/', token())
+    .then(({ data }) => { apps = data })
+    .catch(catchErr)
+}
+
+function randomString(e = 10) {
+  const t = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnoprstuvwxyz1234567890'
+  let n = ''
+  for (let i = 0; i < e; i++) n += t.charAt(Math.floor(Math.random() * 62))
+  return n
+}
+
+window.add = id => {
+  edit = { id }
+  open = false
+}
+
+function select (id) {
+  edit = id ? ({...apps[id]}) : ({ id: randomString() })
+  open = false
+}
+
+function upsert (app) {
+  delete app.secret
+  delete app.sk
+  apps[app.id] = app
+}
+
+function remove (app) {
+  console.log(app)
+  delete apps[app.id]
+  edit = null
+}
 </script>
 
 <style scoped>
-div.admin {
+div.main {
+  position: relative;
+  left: 0;
+  display: flex;
   width: 100%;
-  min-height: 100vh;
+  height: 100%;
+  transition: all 0.5s ease;
+}
+div.list {
+  min-width: 240px;
+  width: 240px;
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
 }
 div.app {
   display: flex;
-  justify-content: space-between;
   align-items: center;
   cursor: pointer;
+}
+div.edit {
+  position: relative;
+  background-color: #f1f2f3;
+  flex-grow: 1;
+  height: 100vh;
+  overflow-y: auto;
+  overflow-x: hidden;
+}
+div.edit * {
+  min-width: 300px;
 }
 </style>
