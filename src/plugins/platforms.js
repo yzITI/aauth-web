5a0406d6c315c77fdf06bcbfc2afc8b679a277ca
Phantomlsh <phantomlsh@outlook.com>
2021-05-07 15:35:15 +0800

    Update redirect endpoint

--- src/plugins/platforms.js
+++ src/plugins/platforms.js
@@ -5,11 +5,11 @@ export default {
     icon: '/platforms/dingtalk.png',
     go: (app, state) => {
       if (navigator.userAgent.includes('DingTalk')) {
-        window.location.href = 'https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_auth&redirect_uri=https%3A%2F%2Faauth.link%2Freenter.html&state=DINGTALK$$' + app + '$$' + state
+        window.location.href = 'https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_auth&redirect_uri=https%3A%2F%2Fcn.aauth.link%2Freenter.html&state=DINGTALK$$' + app + '$$' + state
       } else {
         window.location.href = window.innerWidth < 800
-          ? 'https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_login&redirect_uri=https%3A%2F%2Faauth.link%2Freenter.html&state=' + state
-          : 'https://oapi.dingtalk.com/connect/qrconnect?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_login&redirect_uri=https%3A%2F%2Faauth.link%2Freenter.html&state=DINGTALK$$' + app + '$$' + state
+          ? 'https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_login&redirect_uri=https%3A%2F%2Fcn.aauth.link%2Freenter.html&state=' + state
+          : 'https://oapi.dingtalk.com/connect/qrconnect?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_login&redirect_uri=https%3A%2F%2Fcn.aauth.link%2Freenter.html&state=DINGTALK$$' + app + '$$' + state
       }
     }
   },
@@ -18,7 +18,7 @@ export default {
     color: '#B71C1C',
     icon: '/platforms/qq.png',
     go: (app, state) => {
-      window.location.href = 'https://graph.qq.com/oauth2.0/authorize?client_id=101907335&response_type=code&redirect_uri=https%3A%2F%2Faauth.link%2Freenter.html&state=QQ$$' + app + '$$' + state
+      window.location.href = 'https://graph.qq.com/oauth2.0/authorize?client_id=101907335&response_type=code&redirect_uri=https%3A%2F%2Fcn.aauth.link%2Freenter.html&state=QQ$$' + app + '$$' + state
     }
   },
   GITHUB: {
