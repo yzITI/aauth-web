57936be8f987c4c380851cb01a726c3564996a17
Phantomlsh <phantomlsh@outlook.com>
2021-11-01 17:48:51 -0700

    Add window.opener

--- src/views/Explode.vue
+++ src/views/Explode.vue
@@ -15,17 +15,28 @@ const SS = window.sessionStorage, LS = window.localStorage
 
 if (!SS[id]) router.push('/launch/' + id)
 else axios.get('/auth/' + id, { headers: { token: SS[id] } })
-  .then(({ data }) => {
-    const type = data.token ? 'token' : 'code'
-    const value = data.token || data.code
-    let url = `${data.redirect}?${type}=${value}`
-    if (route.query.state) url += '&state=' + route.query.state
-    window.location.href = url
-  })
+  .then(resp => { go(resp.data) })
   .catch(err => {
     tip = err.response ? err.response.data : '网络错误'
     SS.removeItem(id)
     LS.removeItem(id)
     if (route.query.remember) setTimeout(() => { router.push('/launch/' + id) }, 800)
   })
+
+function go (data) {
+  if (window.opener) {
+    const msg = {}
+    if (data.token) msg.token = data.token
+    if (data.code) msg.code = data.code
+    if (route.query.state) msg.state = route.query.state
+    window.opener.postMessage(msg, data.redirect)
+    setTimeout(window.close, 1000)
+  } else {
+    const type = data.token ? 'token' : 'code'
+    const value = data.token || data.code
+    let url = `${data.redirect}?${type}=${value}`
+    if (route.query.state) url += '&state=' + route.query.state
+    window.location.href = url
+  }
+}
 </script>
