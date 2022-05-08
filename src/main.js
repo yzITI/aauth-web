3258cede684d16713d6bab246ca5322c5cf83d2c
Phantomlsh <phantomlsh@outlook.com>
2022-05-07 19:48:30 -0700

    add srpc

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router.js'
 import './index.css'
+import srpc from './plugins/srpc.js'
+window.srpc = srpc
 
 createApp(App).use(router).mount('#app')
