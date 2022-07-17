d8d71506c1ef1d774c4c5ea3f5aaa1a40aa3a3cd
Phantomlsh <phantomlsh@outlook.com>
2022-07-17 07:44:29 -0700

    Upgrade dependencies

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue({ script: { refSugar: true } })]
+  plugins: [vue({ reactivityTransform: true })]
 })
