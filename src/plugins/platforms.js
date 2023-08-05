5f8b26333c3e798e0153fcdbabc1142f4cfb258b
Phantomlsh <phantomlsh@outlook.com>
2023-08-05 13:50:27 +0800

    Update Dingtalk callback

--- src/plugins/platforms.js
+++ src/plugins/platforms.js
@@ -6,11 +6,11 @@ export default {
     qrcode: true,
     go: (app, state, qrcode) => {
       if (navigator.userAgent.includes('DingTalk')) {
-        window.location.href = 'https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_auth&redirect_uri=https%3A%2F%2Fcn.aauth.link%2Freenter.html&state=DINGTALK$$' + app + '$$' + state
+        window.location.href = 'https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_auth&redirect_uri=https%3A%2F%2Fauth.njsc.ltd%2Freenter.html&state=DINGTALK$$' + app + '$$' + state
       } else {
         window.location.href = qrcode
-          ? 'https://oapi.dingtalk.com/connect/qrconnect?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_login&redirect_uri=https%3A%2F%2Fcn.aauth.link%2Freenter.html&state=DINGTALK$$' + app + '$$' + state
-          : 'https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_login&redirect_uri=https%3A%2F%2Fcn.aauth.link%2Freenter.html&state=DINGTALK$$' + app + '$$' + state
+          ? 'https://oapi.dingtalk.com/connect/qrconnect?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_login&redirect_uri=https%3A%2F%2Fauth.njsc.ltd%2Freenter.html&state=DINGTALK$$' + app + '$$' + state
+          : 'https://oapi.dingtalk.com/connect/oauth2/sns_authorize?appid=dingoaprellztzihaw4y82&response_type=code&scope=snsapi_login&redirect_uri=https%3A%2F%2Fauth.njsc.ltd%2Freenter.html&state=DINGTALK$$' + app + '$$' + state
       }
     }
   },
